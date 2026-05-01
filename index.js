025d849f1cdf33fbe6d10c11cdf8e6bfac86acae
Linuxgeekforgeeks <mahmoudalisaaa@gmail.com>
2026-04-30 17:31:01 -1200

    am trying to test if the routes works and params

--- index.js
+++ index.js
@@ -2,7 +2,10 @@ import express from "express"
 import dotevn from "dotenv"
 
 const app=express()
-
+app.get("/test/:name",(req,res)=>{
+    res.send(`Your name is ${req.params.name}`)
+}
+)
 const PORT=process.env.PORT|| 4000
 
 app.listen(PORT,()=>{
